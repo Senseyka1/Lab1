836f04234f787783884e49911efd61d313210b4c
Senseyka1 <misa.ripta123@gmail.com>
2021-02-24 18:47:55 +0200

    first commit1

--- source.c
+++ source.c
@@ -6,16 +6,18 @@
 
 	setlocale (LC_CTYPE, "ua");
 
-	int ceas, min, sec;
+	int ceas,min, sec;
 	int n, m = 60, h = 3600 , s = 60;
-	printf("Час введення: ");
+	printf("Ceas vedennea:");
 	scanf("%d",&n);
 	
 	ceas = n / h;
 	min =  (n - h) / m;
 	sec =  n - h - m;
 
-	printf("Година = %d \nХвилини = %d \nСекунди = %d \n",ceas, min, sec );
+	printf("Godina = %d \nXvuluna = %d \nSecyndu = %d \n",ceas, min, sec );
 
 	return 0;
 }
+  
+
